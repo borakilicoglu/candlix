015cd416ec8206da57c0c9df26893423dca19c64
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-12-05 14:46:18 +0300

    Make project great again

--- app/components/patterns/threeCandlePatterns/ThreeOutsideDown.tsx
+++ app/components/patterns/threeCandlePatterns/ThreeOutsideDown.tsx
@@ -6,39 +6,27 @@ export default function ThreeOutsideDown({ title }: { title?: string }) {
     <PatternWrapper title={title}>
       <Candle
         color="green"
-        wickTop={15}
-        bodyWidth={2}
-        bodyHeight={5}
-        wickBottom={20}
-        offsetX={0}
-        offsetY={15}
-      />
-      <Candle
-        color="green"
         wickTop={8}
-        bodyHeight={35}
+        bodyHeight={15}
         wickBottom={8}
         offsetX={0}
-        offsetY={-5}
-        stars={[{ position: "top-left" }]}
+        offsetY={5}
       />
       <Candle
         color="red"
-        wickTop={8}
-        bodyHeight={35}
-        wickBottom={8}
+        wickTop={10}
+        bodyHeight={55}
+        wickBottom={10}
         offsetX={0}
-        offsetY={-10}
-        stars={[{ position: "bottom-right" }]}
+        offsetY={5}
       />
       <Candle
         color="red"
-        wickTop={15}
-        bodyWidth={2}
-        bodyHeight={5}
-        wickBottom={20}
+        wickTop={10}
+        bodyHeight={40}
+        wickBottom={10}
         offsetX={0}
-        offsetY={15}
+        offsetY={24}
       />
     </PatternWrapper>
   );
